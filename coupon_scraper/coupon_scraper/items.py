2c93a41feb6ac42150179a245d1e0b9e4de9f289
Almas Akchabayev <almas@Almass-MacBook-Pro.local>
2014-12-30 23:38:40 +0600

    Chocolife scraper initial work

--- coupon_scraper/coupon_scraper/items.py
+++ coupon_scraper/coupon_scraper/items.py
@@ -7,7 +7,23 @@
 
 import scrapy
 
+class MyItem(scrapy.Item):
 
-class PersonItem(DjangoItem):
-    # fields for this item are automatically created from the django model
-    django_model = Person
+   # ... other item fields ...
+   image_url = scrapy.Field()
+   title = scrapy.Field()
+   summary_front=scrapy.Field()
+   old_price=scrapy.Field()
+   new_price = scrapy.Field()
+   end_date_time = scrapy.Field()
+   a_number_of_purchases = scrapy.Field()
+   discount_percentage = scrapy.Field()
+   discount_text = scrapy.Field() #up to, from, etc.
+   description = scrapy.Field()
+   category = scrapy.Field()
+   company=scrapy.Field()
+   tag = scrapy.Field()
+   deal_url = scrapy.Field()
+   website = scrapy.Field()
+   address = scrapy.Field()
+   info = scrapy.Field()
--- coupon_scraper/coupon_scraper/spiders/chocolife_spider.py
+++ coupon_scraper/coupon_scraper/spiders/chocolife_spider.py
@@ -3,17 +3,53 @@ from scrapy.http import Request
 from scrapy.spider import Spider
 from scrapy.selector import Selector
 
-from arbitrage_scraper.items import FootballMatchContainerItem
+from coupon_scraper.items import MyItem
 
 
-class ArbitrageOddsSpider(Spider):
-    name = "arbitrage"
+class MySpider(Spider):
+    name = 'chocolife'
     allowed_domains = [
         'chocolife.me'
     ]
     start_urls = [
-        'http://www.chocolife.me'
+        'http://www.chocolife.me',
     ]
 
     def parse(self, response):
-        selector = Selector(response)
+        sel = Selector(response)
+        category_urls = sel.xpath('//ul[@class="b-deals__menunav__select"]/li/a/@href').extract()
+        for url in category_urls[1:8]:
+            if 'http' not in url:
+                url = 'http://www.chocolife.me' + url
+            yield Request(url, callback=self.parse_deal)
+
+    def parse_deal(self, response):
+        sel = Selector(response)
+        deals=sel.xpath('//li[@class="b-deal"]')
+        count=0
+
+        for deal in deals[:10]:
+            if 'display: none' not in deal.xpath('//@style').extract()[0]:
+                item=MyItem()
+                item['discount_text']=sel.xpath('//span[@class="e-deal__discount"]/text()').extract()[count]
+                item['title']=sel.xpath('//h2[@class="e-deal__title"]/text()').extract()[count]
+                item['summary_front']=sel.xpath('//p[@class="e-deal__text"]/text()').extract()[count]
+                item['a_number_of_purchases']=sel.xpath('//span[@class="e-deal__link"]/text()').extract()[count]
+                item['image_url']='http://www.chocolife.me'+sel.xpath('//img[@class="e-deal__img lazy"]/@data-original').extract()[count]
+                item['old_price']=sel.xpath('//span[@class="e-deal__price e-deal__price--old "]/text()').extract()[count]
+                item['new_price']=sel.xpath('//span[@class="e-deal__price "]/text()').extract()[count]
+                item['deal_url']=sel.xpath('//a[@class="e-link--deal"]/@href').extract()[count]
+                count+=1
+                request = Request(
+                    item['deal_url'],
+                    callback=self.parse_deal_info
+                )
+                request.meta['item'] = item
+                yield request
+
+    def parse_deal_info(self, response):
+        item = response.meta['item']
+        sel = Selector(response)
+        item['address']=sel.xpath('//li[@class="e-offer__feature "]/text()').extract()[0]
+        item['info']=sel.xpath('//p[@class="e-offer__description"]/text()').extract()[0]
+        yield item
